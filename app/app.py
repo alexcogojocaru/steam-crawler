b74906d17f07fe053951673f1ff8082ef44a7890
Alex Cojocaru <alex.cojocaru73@gmail.com>
2022-01-10 20:57:54 +0200

    upload services

---
+++ app/app.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+import requests
+import bs4
+import re
+import json
+import redis
+import os
+import uvicorn
+
+HEADERS                 = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+REDIS_WAITING_QUEUE     = 'API_SERVICE_SEARCH_WAITING'
+REDIS_PROCESSED_QUEUE   = 'API_SERVICE_SEARCH_PROCESSED'
+REDIS_HOST              = os.environ.get('REDIS_HOST') if os.environ.get('REDIS_HOST') is not None else 'localhost'
+
+app = FastAPI()
+redis_client = redis.Redis(host=REDIS_HOST, port=6379)
+
+def get_data(data, element, classname, pattern):
+    soup = bs4.BeautifulSoup(data, 'html.parser')
+    searchdata = str(soup.find_all(element, { 'class': classname })[0])
+
+    value = re.search(pattern, searchdata)
+    return value.group(1).split(' ')[0]
+
+@app.post('/api')
+async def get_urls(keywords: List[str]):
+    for key in keywords:
+        redis_client.rpush(REDIS_WAITING_QUEUE, key)
+
+    cache = {}
+    while len(cache.keys()) != len(keywords):
+        value = redis_client.rpop(REDIS_PROCESSED_QUEUE)
+        if value:
+            value = value.decode()
+            link = json.loads(value)
+            keyword = link['value']
+            if keyword not in cache:
+                cache[keyword] = [link]
+            else:
+                cache[keyword].append(link)
+
+    return cache
+
+@app.post('/api/game/{appid}')
+async def get_game(appid: str):
+    req = requests.get(f'https://steamcommunity.com/app/{appid}', headers=HEADERS)
+
+    players_ingame      = get_data(req.text, 'span', 'apphub_NumInApp', r'<span class="apphub_NumInApp">(.*)</span>')
+    original_price      = get_data(req.text, 'div', 'discount_original_price', r'<div class="discount_original_price">(.*)</div>')
+    discounted_price    = get_data(req.text, 'div', 'discount_final_price', r'<div class="discount_final_price">(.*)</div>')
+
+    return {
+        'players_ingame':   players_ingame,
+        'original_price':   original_price,
+        'discounted_price': discounted_price,
+        'time':             datetime.now().timestamp()
+    }
+
+if __name__ == '__main__':
+    uvicorn.run("app:app", host='localhost', port=8000, reload=True)
---
+++ search/search_service.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+import bs4
+import json
+import os
+import redis
+import re
+import requests
+import time
+
+
+REDIS_HOST              = os.environ.get('REDIS_HOST') if os.environ.get('REDIS_HOST') is not None else 'localhost'
+redis_client            = redis.Redis(host=REDIS_HOST, port=6379)
+REDIS_WAITING_QUEUE     = 'API_SERVICE_SEARCH_WAITING'
+REDIS_PROCESSED_QUEUE   = 'API_SERVICE_SEARCH_PROCESSED'
+HEADERS                 = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+if __name__ == '__main__':
+    while True:
+        value = redis_client.rpop(REDIS_WAITING_QUEUE)
+
+        if value is None:
+            continue
+
+        print(f'[{datetime.now()}] {value}')
+        value = value.decode().replace(" ", "+")
+        link = f'https://www.google.com/search?q=steam+{value}'
+        req = requests.get(link, headers=HEADERS)
+        soup = bs4.BeautifulSoup(req.text, 'html.parser')
+        names = soup.find_all('div', {'class': 'yuRUbf'})
+
+        for name in names:
+            url = name.find('a')['href']
+            temp = re.search(r'https://store.steampowered.com/app/(.*)', url)
+            if temp:
+                keywords = temp.group(1).split('/')
+                if len(keywords) != 1:
+                    app, name = keywords[0:2]
+                    if 'romanian' not in temp.group():
+                        data = json.dumps({
+                            'value': value,
+                            'appid': app,
+                            'appurl': temp.group(),
+                            'appname': name
+                        })
+                        redis_client.rpush(REDIS_PROCESSED_QUEUE, data)
