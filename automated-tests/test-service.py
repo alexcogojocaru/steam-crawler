6cf02d5b7e80051cda9726e01e0bdb5813338b77
alexcogojocaru <alex.cojocaru73@gmail.com>
2022-01-16 21:07:48 +0200

    unittests and updated app

--- automated-tests/test-service.py
+++ automated-tests/test-service.py
@@ -2,21 +2,21 @@ import requests
 import unittest
 
 
-APPLICATION_HOST = '192.168.100.2'
-APPLICATION_PORT = 8000
+APPLICATION_HOST = '192.168.100.23'
+APPLICATION_PORT = 4000
 
 class APITest(unittest.TestCase):
     def test_data_integrity(self):
-        req = requests.post(f'http://{APPLICATION_HOST}:{APPLICATION_PORT}/api/gamesearch', json=['batman', 'god of war'])
+        req = requests.post(f'http://{APPLICATION_HOST}:{APPLICATION_PORT}/api/gamecode', json=['batman', 'god of war'])
         self.assertEqual(type(req.json()), dict)
 
     def test_data_count(self):
-        req = requests.post(f'http://{APPLICATION_HOST}:{APPLICATION_PORT}/api/gamesearch', json=['batman'])
+        req = requests.post(f'http://{APPLICATION_HOST}:{APPLICATION_PORT}/api/gamecode', json=['batman'])
         self.assertLess(len(req.json()['batman']), 10)
 
     def test_invalid_game_id(self):
-        req = requests.post(f'http://{APPLICATION_HOST}:{APPLICATION_PORT}/api/game/0000000000000000000000')
-        self.assertEqual(req.text, '\"INVALID APPLICATION ID\"')
+        req = requests.get(f'http://{APPLICATION_HOST}:{APPLICATION_PORT}/api/game/0000000000000000000000')
+        self.assertEqual(req.text, '\"INVALID_APP_ID\"')
 
 if __name__ == '__main__':
     unittest.main()
